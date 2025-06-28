84ac9a6b264593f07ec54012ac3ef35717ab8d00
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2025-06-28 18:19:14 +0700

    format

--- src/components/Navigation/Navbar.svelte
+++ src/components/Navigation/Navbar.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-import DesktopNavigation from "./DektopNavigation.svelte";
-import type { Item } from "./items";
-import { items } from "./items";
-import MobileNavigation from "./MobileNavigation.svelte";
+  import DesktopNavigation from "./DektopNavigation.svelte";
+  import type { Item } from "./items";
+  import { items } from "./items";
+  import MobileNavigation from "./MobileNavigation.svelte";
 
-function _setActive() {
-	// @ts-ignore
-	const currentlyActive = mostVisible(document.querySelectorAll(".section"));
+  function _setActive() {
+    // @ts-ignore
+    const currentlyActive = mostVisible(document.querySelectorAll(".section"));
 
-	if (currentlyActive?.id) {
-		active = currentlyActive.id;
-	}
-}
+    if (currentlyActive?.id) {
+      active = currentlyActive.id;
+    }
+  }
 
-export let active: Item = "home";
+  export let active: Item = "home";
 </script>
 
 <svelte:window
